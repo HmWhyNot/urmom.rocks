5af939d37ab0b7bc1191acb0ab1c94805077b230
HmWhyNot <shane.conger@cdw.com>
2022-10-25 17:43:41 -0700

    nice site

--- ClientApp/src/components/MainPage.vue
+++ ClientApp/src/components/MainPage.vue
@@ -1,6 +1,10 @@
 <template>
-    <v-container>
-      <h1>no u</h1>
+    <v-container fluid class="fill-height">
+      <v-row fluid class="fill-height">
+          <v-col fluid align="center" class="fill-height">
+              <h1>no u</h1>
+          </v-col>
+        </v-row>
     </v-container>
 </template>
 
