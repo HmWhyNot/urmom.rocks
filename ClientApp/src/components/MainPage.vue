f8f1f2071249009a8adc05696b0c957df619ab02
HmWhyNot <HmWhyNot@users.noreply.github.com>
2022-10-27 08:07:44 -0700

    coo

--- ClientApp/src/components/MainPage.vue
+++ ClientApp/src/components/MainPage.vue
@@ -1,13 +1,13 @@
 <template>
     <v-sheet border class="fill-height">
-    <v-container fluid class="fill-height">
-        <v-sheet border class="fill-height">
-            <v-row fluid justify="center" align="center" class="fill-height">
+        <v-container fluid class="fill-height">
+            <v-sheet border class="fill-height">
+                <v-row fluid justify="center" align="center" class="fill-height">
                     <h1>no u</h1>
-            </v-row>
-        </v-sheet>
-    </v-container>
-</v-sheet>
+                </v-row>
+            </v-sheet>
+        </v-container>
+    </v-sheet>
 </template>
 
 <script setup lang='ts'>
