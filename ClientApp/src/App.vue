add89c130748bb86fbd35f36f150e6e2d92658dc
HmWhyNot <HmWhyNot@users.noreply.github.com>
2022-10-27 00:55:13 -0700

    cool

--- ClientApp/src/App.vue
+++ ClientApp/src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <v-app>
-    <v-app-bar app color="grey-lighten-1" density="compact" :elevation="elevation">
-      <v-app-bar-nav-icon @mouseover="mom = 'WOW'" @mouseleave="mom = 'MOM'"> {{ mom }} </v-app-bar-nav-icon>
+    <v-app-bar app color="grey-lighten-1" border="barBorder" density="compact" :elevation="elevation">
+      <v-app-bar-nav-icon @click="dialog = true" @mouseover="mom = 'WOW'" @mouseleave="mom = 'MOM'"> {{ mom }} </v-app-bar-nav-icon>
       <v-app-bar-title align="center">
         <b>{{ title }}</b>
       </v-app-bar-title>
     </v-app-bar>
 
-    <v-dialog v-model="dialog" :disabled="wow" :fullscreen="mob.mobile.value">
-      <v-btn @click="wow = true">
+    <v-dialog v-model="dialog" :fullscreen="mobile">
+      <v-btn @click="dialog = false">
         Hi
       </v-btn>
     </v-dialog>
@@ -31,19 +31,13 @@ import { useDisplay } from 'vuetify'
 import HelloWorld from './components/HelloWorld.vue'
 import MainPage from './components/MainPage.vue'
 
-const wow = ref<boolean>(true)
-const dialog = true;
-const elevation = 10;
-const title = 'oofgottem';
+const dialog = ref<boolean>(false);
+const barBorder = ref<number>(3);
+const elevation = ref<number>(10);
+const title = ref<string>('oofgottem');
 const mom = ref<string>('MOM');
 const { mobile } = useDisplay();
-const mob = useDisplay();
-console.log(mobile);
-mob.mobileBreakpoint = 'sm';
-console.log(mob.mobile);
-// mob.mobile = true;
-console.log(mob.mobile);
-console.log(wow.value);
+console.log(useDisplay());
 
 
 </script>
--- ClientApp/src/components/MainPage.vue
+++ ClientApp/src/components/MainPage.vue
@@ -1,14 +1,17 @@
 <template>
+    <v-sheet border class="fill-height">
     <v-container fluid class="fill-height">
-      <v-row fluid class="fill-height">
-          <v-col fluid align="center" class="fill-height">
-              <h1>no u</h1>
-          </v-col>
-        </v-row>
+        <v-sheet border class="fill-height">
+            <v-row fluid justify="center" align="center" class="fill-height">
+                    <h1>no u</h1>
+            </v-row>
+        </v-sheet>
     </v-container>
+</v-sheet>
 </template>
 
 <script setup lang='ts'>
+import { ref } from 'vue'
 
 </script>
 
