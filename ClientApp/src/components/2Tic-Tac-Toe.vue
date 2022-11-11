4f383f4288155d1103dbdf6474885da7d3589aff
HmWhyNot <shane.conger@cdw.com>
2022-11-10 17:43:22 -0800

    dfsdf

--- ClientApp/src/components/2Tic-Tac-Toe.vue
+++ ClientApp/src/components/2Tic-Tac-Toe.vue
@@ -1,11 +1,17 @@
 <template>
     <v-container>
-        <canvas id="ttt">Tic Tac Toe</canvas>
+        <canvas ref="ttt" id="ttt">Tic Tac Toe</canvas>
     </v-container>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
 
+const root = ref();
+
+onMounted(() => {
+    console.log(root.value);
+})
 </script>
 
 <style>
