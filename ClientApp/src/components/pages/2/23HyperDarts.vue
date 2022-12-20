3409de47113c072bc56884a001ffcee6e6aa90ab
HmWhyNot <shane.conger@cdw.com>
2022-12-19 17:40:57 -0800

    route

---
+++ ClientApp/src/components/pages/2/23HyperDarts.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container class="fill-height">
+    <v-sheet>
+
+
+
+    </v-sheet>
+  </v-container>
+</template>
--- ClientApp/src/plugins/router.ts
+++ ClientApp/src/plugins/router.ts
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import NoPage from '@/components/NoPage.vue'
-import MainPage from '@/components/0MainPage.vue'
-import Papa from '@/components/1Papa.vue'
-import TicTacToe from '@/components/2Tic-Tac-Toe.vue'
-import CircleGame from '@/components/3CircleGame.vue'
+import NoPage from '@/components/pages/NoPage.vue'
+import MainPage from '@/components/pages/0/0MainPage.vue'
+import Papa from '@/components/pages/0/1Papa.vue'
+import TicTacToe from '@/components/pages/2/21Tic-Tac-Toe.vue'
+import CircleGame from '@/components/pages/2/22CircleGame.vue'
 
 const routes: RouteRecordRaw[] = [
     { path: '/:noPage+', name: 'NoPage', component: NoPage },
@@ -12,6 +12,18 @@ const routes: RouteRecordRaw[] = [
     { path: '/tic-tac-toe', name: 'TicTacToe', component: TicTacToe },
     { path: '/circle-game', name: 'CircleGame', component: CircleGame },
 ]
+const pages = import.meta.glob('../components/pages/**').map();
+console.log(pages);
+// function loadRoutes() {
+//     const pages = import.meta.glob('../components/pages/**');
+//     // const pageList = pages.
+//     for (const path in pages) {
+//         console.log(path);
+//     }
+//     console.log('glob');
+//     console.log(pages);
+// }
+// loadRoutes();
 
 export default createRouter({
     history: createWebHistory(),
