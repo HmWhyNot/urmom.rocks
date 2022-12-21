b32c83dcff71cfff458c9d93e08542f678e67558
HmWhyNot <shane.conger@cdw.com>
2022-12-20 17:40:21 -0800

    onestecloser

--- ClientApp/src/plugins/router.ts
+++ ClientApp/src/plugins/router.ts
@@ -5,13 +5,21 @@ import Papa from '@/components/pages/0/1Papa.vue'
 import TicTacToe from '@/components/pages/2/1Tic-Tac-Toe.vue'
 import CircleGame from '@/components/pages/2/2CircleGame.vue'
 
+// const routes: RouteRecordRaw[] = [
+//     { path: '/:noPage+', name: 'NoPage', component: NoPage },
+//     { path: '/', name: 'MainPage', component: MainPage },
+//     { path: '/papa', name: 'Papa', component: Papa },
+//     { path: '/tic-tac-toe', name: 'TicTacToe', component: TicTacToe },
+//     { path: '/circle-game', name: 'CircleGame', component: CircleGame },
+// ]
+
 const basePageDir: string = '../components/pages/';
 const pages: Record<string, () => Promise<unknown>> = import.meta.glob('../components/pages/*/**.vue');
 const pageList: string[] = Object.keys(pages).map(a => a.replace(basePageDir, ''));
 let pageObj: {[key: string]: object[]} = {};
 for (const k in pages) {
     const p = pages[k];
-    const m = k.replace(basePageDir, '').match(/(\d)\/(\d)(.*)\.vue/) ?? [''];
+    const m = k.replace(basePageDir, '').match(/(\d).*\/(\d)(.*)\.vue/) ?? [''];
     if (!(m[1] in pageObj)) { pageObj[m[1]] = [] };
     pageObj[m[1]][Number(m[2])] = { [m[3]]: p};
 }
