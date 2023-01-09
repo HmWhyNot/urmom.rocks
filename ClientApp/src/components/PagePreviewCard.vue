84a31b81d66836e499df1779b42c8153b327fce4
HmWhyNot <HmWhyNot@users.noreply.github.com>
2023-01-08 23:31:04 -0800

    rrrr

---
+++ ClientApp/src/components/PagePreviewCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <v-sheet>
+      <slot>poo</slot>
+    </v-sheet>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+</style>
--- ClientApp/src/components/pages/2Games/0Games.vue
+++ ClientApp/src/components/pages/2Games/0Games.vue
@@ -6,12 +6,20 @@
         <router-view v-slot="{ Component, route }">
           
           <v-container v-if="route.matched[0].name == route.name" ref="con" id="con" class="ma-0 pa-0 fill-height">
-            <v-row justify="center" class="fill-height">
-              Implement list of games here
+            <v-row justify="center" no-gutters class="fill-height">
+              <v-col v-for="game in route.matched[0].children" :key="game.name" cols="4" class="pa-16">
+                <router-link class="nav-link" tag="div" :to="game" replace>
+                  <v-sheet>
+                    <v-container>
+                      <PagePreviewCard>{{ game.name }}</PagePreviewCard>
+                    </v-container>
+                  </v-sheet>
+                </router-link>
+              </v-col>
             </v-row>
           </v-container>
 
-          <component v-else :is="Component"></component>
+          <component v-else :is="Component"/>
 
         </router-view>
       </transition>
@@ -19,3 +27,12 @@
     </v-sheet>
   </v-container>
 </template>
+
+<script setup lang="ts">
+import PagePreviewCard from '@/components/PagePreviewCard.vue';
+
+
+</script>
+
+<style>
+</style>
